9fd941b1219c50659589ab43bc75d45bd5e3d1cf
masoom <masoomshaikh4874@gmail.com>
2023-10-05 04:17:16 +0530

    submission completed

--- src/App.js
+++ src/App.js
@@ -8,34 +8,87 @@ import mockHotelData from "./components/store";
 import HeaderText from "./components/HeaderText";
 import Card from "./components/Card";
 function App() {
-  const [visibleCards, setVisibleCards] = useState(6); // Initially display 6 cards
-  const cardsToAdd = 3; // Number of cards to add when "Show More" is clicked
+  const [visibleCards, setVisibleCards] = useState(6);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const cardsToAdd = 3;
 
   const handleShowMore = () => {
     setVisibleCards((prevVisibleCards) => prevVisibleCards + cardsToAdd);
+  };
+
+  const filterHotelData = () => {
+    if (selectedCity) {
+      return mockHotelData.filter((hotel) => hotel.city === selectedCity);
+    }
+    return mockHotelData.slice(0, visibleCards);
   };
   return (
     <div className="App">
       <div className="d-flex flex-column justify-content-center align-items-center">
         <HeaderText />
         <div className="main-width d-flex gap-3 mb-3">
-          <button type="button" class="btn button-color rounded-4">
+          <button
+            type="button"
+            className={`btn button-color rounded-4 ${
+              selectedCity === "New York" ? "active" : ""
+            }`}
+            onClick={() => setSelectedCity("New York")}
+            style={{
+              background: selectedCity === "New York" ? "blue" : "",
+              border: selectedCity === "New York" ? "none" : "1px solid #ccc",
+              color: selectedCity === "New York" ? "white" : "black",
+            }}
+          >
             New York
           </button>
-          <button type="button" class="btn button-color rounded-4">
+          <button
+            type="button"
+            className={`btn button-color rounded-4 ${
+              selectedCity === "Mumbai" ? "active" : ""
+            }`}
+            onClick={() => setSelectedCity("Mumbai")}
+            style={{
+              background: selectedCity === "Mumbai" ? "blue" : "",
+              border: selectedCity === "Mumbai" ? "none" : "1px solid #ccc",
+              color: selectedCity === "Mumbai" ? "white" : "black",
+            }}
+          >
             Mumbai
           </button>
-          <button type="button" class="btn button-color rounded-4">
+          <button
+            type="button"
+            className={`btn button-color rounded-4 ${
+              selectedCity === "Paris" ? "active" : ""
+            }`}
+            onClick={() => setSelectedCity("Paris")}
+            style={{
+              background: selectedCity === "Paris" ? "blue" : "",
+              border: selectedCity === "Paris" ? "none" : "1px solid #ccc",
+              color: selectedCity === "Paris" ? "white" : "black",
+            }}
+          >
             Paris
           </button>
-          <button type="button" class="btn button-color rounded-4">
+          <button
+            type="button"
+            className={`btn button-color rounded-4 ${
+              selectedCity === "London" ? "active" : ""
+            }`}
+            onClick={() => setSelectedCity("London")}
+            style={{
+              background: selectedCity === "London" ? "blue" : "",
+              border: selectedCity === "London" ? "none" : "1px solid #ccc",
+              color: selectedCity === "London" ? "white" : "black",
+            }}
+          >
             London
           </button>
         </div>
 
         {/* //content start here */}
         <div className="main-width">
-          <CardList visibleCards={visibleCards} />
+          <CardList visibleCards={visibleCards} hotelData={filterHotelData()} />
         </div>
         <div className="my-4">
           {visibleCards < mockHotelData.length && (
